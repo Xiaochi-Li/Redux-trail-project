48e50d569804cbead2baa0f0281d4742c0048c80
SeanLi <294188719@qq.com>
2018-03-23 10:09:44 +1000

    refactor in react way

---
+++ src/middleware/logger.js
@@ -0,0 +1,12 @@
+
+let logger =null;
+logger = (store) => (next) => (action) => {
+	console.group(action.type)
+	console.log('The action: ', action)
+	const result = next(action)
+	console.log('The new state: ', store.getState())
+	console.groupEnd();
+	return result
+}
+
+export default logger
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import goals from "./goals";
+import loading from "./loading";
+import todos from "./todos";
+import {combineReducers} from'redux'
+
+export default combineReducers({
+	todos,
+	loading,
+	goals,
+})
